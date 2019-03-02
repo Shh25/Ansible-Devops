3b05040d9cf2a52a30b68bf2089bef47693513c5
Shefali Agarwal <sdagarwa@ncsu.edu>
2019-03-02 15:30:40 -0500

    Role Fuzzer: Basic fuzzer script with itrust

--- ansible-srv/roles/fuzzer/files/fuzzer_script.py
+++ ansible-srv/roles/fuzzer/files/fuzzer_script.py
@@ -2,37 +2,24 @@
 
 import sys
 import os, fnmatch
-import codecs
 itrustPath = sys.argv[1]
 
-# print('1st Argument: ', sys.argv[1])
 def getFilesByPattern(pattern, directoryPath) :
     result = []
     for root, dirs, files in os.walk(directoryPath):
         for name in files:
             if fnmatch.fnmatch(name, pattern):
                 result.append(os.path.join(root, name))
-    # print(result)
     return result
     
-def addFuzzer(itrustPath) :
-    javaFiles = getFilesByPattern('*.java', itrustPath)
+def addFuzzer(pattern, directoryPath) :
+    javaFiles = getFilesByPattern(pattern, directoryPath)
     for filename in javaFiles:
-        f = codecs.open(filename, encoding='utf-8')
-        contents = f.read()
+        with open(filename, 'r') as inputfile:
+            newText = inputfile.read().replace('<', '>').replace('==', '!=').replace('0', '1')
 
-        newcontents = contents.replace('>','<').replace('==', '!=').replace('0', '1')
-        f.close()
+        with open(filename, 'w') as outputfile:
+            outputfile.write(newText)
 
-        f = codecs.open(filename,"w", encoding='utf-8')
-        f.write(newcontents)
-        print(f)
-        # with open(f, "r") as inputfile:
-        #     newText = inputfile.read().replace('<', '>')
-        #     print(newText)
 
-        # with open(f, "w") as outputfile:
-        #     outputfile.write(newText)
-
-addFuzzer(itrustPath)
-
+addFuzzer('*.java', itrustPath)
